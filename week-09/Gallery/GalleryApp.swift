594b47c97394e8324453ba5099ec94158141b5d4
Jonathan Parra <58872902+jondeparra@users.noreply.github.com>
2024-01-27 17:05:54 -0500

    Finished Homework 9 short-answer questions

---
+++ week-09/Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Jonathan Parra on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
