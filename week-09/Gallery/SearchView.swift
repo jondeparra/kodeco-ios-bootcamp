952d01dd19ee744b7b6ec3791804029da2a9fe32
Jonathan Parra <58872902+jondeparra@users.noreply.github.com>
2024-02-04 21:20:24 -0800

    Added empty state for SearchView

--- week-09/Gallery/SearchView.swift
+++ week-09/Gallery/SearchView.swift
@@ -14,52 +14,83 @@ struct SearchView: View {
     var body: some View {
         NavigationView {
             GeometryReader { geo in
-                ScrollView {
-                    VStack(spacing: 16.0) {
-                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(minimum: geo.size.width * 0.33, maximum: geo.size.width * 0.5), spacing: 16), count: 2), spacing: 16) {
-                            ForEach(viewModel.photos) { photo in
-                                NavigationLink(destination: DetailView(photo: photo)) {
-                                    AsyncImage(url: URL(string: photo.src.medium ?? photo.src.original)) { image in
-                                        image
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(height: 100)
-                                            .clipped()
-                                    } placeholder: {
-                                        ProgressView()
+                if viewModel.photos.isEmpty {
+                    VStack {
+                        Spacer()
+
+                        VStack(spacing: 16) {
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 36))
+                                .foregroundStyle(Color.accentColor)
+                            Text("Search to view photos")
+                                .frame(maxWidth: .infinity)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 16)
+                        }
+
+                        Spacer()
+                    }
+                } else {
+                    ScrollView {
+                        VStack(spacing: 16.0) {
+                            LazyVGrid(columns: Array(repeating: GridItem(.flexible(minimum: geo.size.width * 0.33, maximum: geo.size.width * 0.5), spacing: 16), count: 2), spacing: 16) {
+                                ForEach(viewModel.photos) { photo in
+                                    NavigationLink(destination: DetailView(photo: photo)) {
+                                        AsyncImage(url: URL(string: photo.src.medium ?? photo.src.original)) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(height: 100)
+                                                .clipped()
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(minWidth: geo.size.width * 0.33, maxWidth: .infinity)
+                                        .cornerRadius(8)
                                     }
-                                    .frame(minWidth: geo.size.width * 0.33, maxWidth: .infinity)
-                                    .cornerRadius(8)
+                                }
+                            }
+
+                            if !viewModel.photos.isEmpty {
+                                VStack(spacing: 8) {
+                                    Button("Load more") {
+                                        withAnimation {
+                                            viewModel.searchPhotos(query: searchTerm)
+                                        }
+                                    }
+                                    .buttonStyle(.borderedProminent)
+
+                                    Spacer()
+
+                                    Button(role: .destructive, action: {
+                                        withAnimation {
+                                            viewModel.clearPhotos()
+                                        }
+                                        searchTerm = ""
+                                    }, label: {
+                                        Text("Clear Search")
+                                    })
                                 }
                             }
                         }
-
+                    }
+                    .scrollIndicators(.hidden)
+                    .refreshable {
                         if !viewModel.photos.isEmpty {
-                            VStack(spacing: 8) {
-                                Button("Load more") {
-                                    viewModel.searchPhotos(query: searchTerm)
-                                }
-                                .buttonStyle(.borderedProminent)
-
-                                Spacer()
-
-                                Button(role: .destructive, action: {
-                                    viewModel.clearPhotos()
-                                    searchTerm = ""
-                                }, label: {
-                                    Text("Clear Search")
-                                })
+                            withAnimation {
+                                viewModel.searchPhotos(query: searchTerm)
                             }
                         }
                     }
                 }
-                .scrollIndicators(.hidden)
             }
             .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search Pexels")
             .padding(.horizontal, 16)
             .onSubmit(of: .search) {
                 if viewModel.photos.isEmpty {
-                    viewModel.searchPhotos(query: searchTerm)
+                    withAnimation {
+                        viewModel.searchPhotos(query: searchTerm)
+                    }
                 }
             }
             .navigationTitle("Pexels Search")
@@ -67,7 +98,9 @@ struct SearchView: View {
                 ToolbarItem(placement: .topBarTrailing) {
                     if !viewModel.photos.isEmpty {
                         Button(role: .destructive, action: {
-                            viewModel.clearPhotos()
+                            withAnimation {
+                                viewModel.clearPhotos()
+                            }
                             searchTerm = ""
                         }, label: {
                             Text("Clear")
